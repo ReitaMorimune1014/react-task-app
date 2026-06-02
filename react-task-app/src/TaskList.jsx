cbc72394f165aebef3defed62cd4e186715dab7b
nekomikandayo <mrmikan.1417@gmail.com>
2026-06-02 22:34:09 +0900

    feat: TaskItemコンポーネントの作成

--- react-task-app/src/TaskList.jsx
+++ react-task-app/src/TaskList.jsx
@@ -1,16 +1,10 @@
+import TaskItem from "./TaskItem";
+
 export default function TaskList({ tasks, onToggle, onDelete }) {
     return (
         <ul>
             {tasks.map((task) => (
-                <li key={task.id}>
-                    <span
-                        onClick={() => onToggle(task.id)}
-                        style={{ textDecoration: task.completed ? "line-through" : "none" }}
-                    >
-                        {task.title}
-                    </span>
-                    <button onClick={() => onDelete(task.id)}>削除</button>
-                </li>
+                <TaskItem key={task.id} task={task} onToggle={onToggle} onDelete={onDelete} />
             ))}
         </ul>
     );
